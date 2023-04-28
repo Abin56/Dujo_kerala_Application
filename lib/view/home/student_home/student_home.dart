dc9fa0bd9b936de1f6ff2d23be0988ed32d6f439
Praveen <praveenpathmakaran@gmail.com>
2023-04-28 13:02:38 +0530

    sruthy merged

--- lib/view/home/student_home/student_home.dart
+++ lib/view/home/student_home/student_home.dart
@@ -1,12 +1,13 @@
 // ignore_for_file: use_key_in_widget_constructors, must_call_super, annotate_overrides, non_constant_identifier_names
 import 'package:dujo_kerala_application/controllers/userCredentials/user_credentials.dart';
-import 'package:dujo_kerala_application/sruthi/user_edit_profile.dart';
 import 'package:dujo_kerala_application/view/home/student_home/student_acc/student_accessories.dart';
 import 'package:dujo_kerala_application/view/colors/colors.dart';
 import 'package:dujo_kerala_application/view/widgets/fonts/google_monstre.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
+import '../../../sruthi/User Edit Profile/user_edit_profile.dart';
 
 class StudentHomeScreen extends StatefulWidget {
   static String routeName = '';
