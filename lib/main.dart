9972149612c3afe974f3d250697fcbbb8f478711
Abin56 <abinjohn8089@gmail.com>
2023-05-17 19:55:19 +0530

    live testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:dujo_kerala_application/controllers/bloc/user_phone_otp/auth_cubit.dart';
-import 'package:dujo_kerala_application/controllers/bloc/user_phone_otp/auth_state.dart';
 import 'package:dujo_kerala_application/firebase_options.dart';
-import 'package:dujo_kerala_application/view/pages/splash_screen/splash_screen.dart';
+import 'package:dujo_kerala_application/view/pages/live_classes/enter_to_live.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'helper/shared_pref_helper.dart';
 
@@ -28,31 +24,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        minTextAdapt: true,
-        splitScreenMode: true,
-        designSize: const Size(423.5294196844927, 945.8823706287004),
-        builder: (context, child) {
-          return BlocProvider(
-              create: (context) => AuthCubit(),
-              child: GetMaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: BlocBuilder<AuthCubit, AuthState>(
-                  buildWhen: (oldState, newState) {
-                    return oldState is AuthInitialState;
-                  },
-                  builder: (context, state) {
-                    if (state is AuthLoggedInState) {
-                      return const SplashScreen();
-                    } else if (state is AuthLoggedOutState) {
-                      return const SplashScreen();
-                    }
-                    return const SplashScreen();
-                  },
-                ),
+    return MaterialApp(
+      home:  LiveClassRoom(
+                        roomID: '2323',
+                    ),
+    );
+    // return ScreenUtilInit(
+    //     minTextAdapt: true,
+    //     splitScreenMode: true,
+    //     designSize: const Size(423.5294196844927, 945.8823706287004),
+    //     builder: (context, child) {
+    //       return BlocProvider(
+    //           create: (context) => AuthCubit(),
+    //           child: GetMaterialApp(
+    //             debugShowCheckedModeBanner: false,
+    //             home: BlocBuilder<AuthCubit, AuthState>(
+    //               buildWhen: (oldState, newState) {
+    //                 return oldState is AuthInitialState;
+    //               },
+    //               builder: (context, state) {
+    //                 if (state is AuthLoggedInState) {
+    //                   return const SplashScreen();
+    //                 } else if (state is AuthLoggedOutState) {
+    //                   return const SplashScreen();
+    //                 }
+    //                 return const SplashScreen();
+    //               },
+    //             ),
 
-                // LoginVerification(),
-              ));
-        });
+    //             // LoginVerification(),
+    //           ));
+    //     });
   }
 }
