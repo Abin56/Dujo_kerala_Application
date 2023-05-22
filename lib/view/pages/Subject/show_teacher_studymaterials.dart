03104ac279b7f6c62ca020d6435f128e0f80d95a
Abin56 <abinjohn8089@gmail.com>
2023-05-22 19:26:12 +0530

    testing pdf viwer

--- lib/view/pages/Subject/show_teacher_studymaterials.dart
+++ lib/view/pages/Subject/show_teacher_studymaterials.dart
@@ -7,133 +7,130 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../sruthi/Subject 2/subject_chapterwise_display.dart';
-import '../pdf_viewer/pdf_viewer.dart'; 
-
+import '../pdf_viewer/pdf_viewer.dart';
 
 class StudyMaterials extends StatelessWidget {
-   StudyMaterials({super.key, required this.subjectID, required this.chapterID}); 
+  StudyMaterials({super.key, required this.subjectID, required this.chapterID});
 
-  String subjectID; 
+  String subjectID;
   String chapterID;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.white70,
+            backgroundColor: Colors.white70,
             appBar: AppBar(
               title: Row(
                 children: const [
-           
                   Text("Study Materials"),
                 ],
               ),
               backgroundColor: adminePrimayColor,
             ),
             body: StreamBuilder(
-              stream: FirebaseFirestore.instance
-        .collection('SchoolListCollection')
-        .doc(UserCredentialsController.schoolId)
-        .collection(UserCredentialsController.batchId!)
-        .doc(UserCredentialsController.batchId)
-        .collection('classes')
-        .doc(UserCredentialsController.classId)
-        .collection('subjects')
-        .doc(subjectID)
-        .collection('Chapters')
-        .doc(chapterID)
-        .collection('StudyMaterials').snapshots(),
-              builder: (context, snapshot) {
-               if(snapshot.hasData){
-                 return ListView.separated(
-                    itemCount: snapshot.data!.docs.length,
-                     separatorBuilder: ((context, index) {
-                    return kHeight10;
-                  }),
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListileCardChapterWidget(leading:const  Image(
-                                    image: NetworkImage(
-                                        "https://media.istockphoto.com/id/926144358/photo/portrait-of-a-little-bird-tit-flying-wide-spread-wings-and-flushing-feathers-on-white-isolated.jpg?b=1&s=170667a&w=0&k=20&c=DEARMqqAI_YoA5kXtRTyYTYU9CKzDZMqSIiBjOmqDNY=")), subtitle:  Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                  
-                                    Padding(
-                                      padding:  EdgeInsets.only(top: 5.h),
-                                      child: GooglePoppinsWidgets(
-                                        fontsize: 15.h,
-                                        text: snapshot.data!.docs[index]['chapterName'],fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    Padding(
-                                        padding:  EdgeInsets.only(top: 5.h),
-                                      child: GooglePoppinsWidgets(
-                                        fontsize: 15.h,
-                                        text:  snapshot.data!.docs[index]['topicName'],
-                                        fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    Padding(
-                                          padding:  EdgeInsets.only(top: 5.h),
-                                      child: GooglePoppinsWidgets(
-                                        fontsize: 14.h,
-                                        text: 'Pdf',
-                                                                        fontWeight: FontWeight.bold
-                                                            
-                                      ),
-                                    ),kHeight10,
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        GooglePoppinsWidgets(
-                                            text: "Posted By :", fontsize: 15.h),
-                                            GooglePoppinsWidgets(
-                                            text: snapshot.data!.docs[index]['uploadedBy'], fontsize: 15.h,fontWeight: FontWeight.bold,),
-                                      ],
-                                    ),
-                                  ],
-                                ), title: Padding(
-                                  padding:  EdgeInsets.only(top: 10.h),
+                stream: FirebaseFirestore.instance
+                    .collection('SchoolListCollection')
+                    .doc(UserCredentialsController.schoolId)
+                    .collection(UserCredentialsController.batchId!)
+                    .doc(UserCredentialsController.batchId)
+                    .collection('classes')
+                    .doc(UserCredentialsController.classId)
+                    .collection('subjects')
+                    .doc(subjectID)
+                    .collection('Chapters')
+                    .doc(chapterID)
+                    .collection('StudyMaterials')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.separated(
+                        itemCount: snapshot.data!.docs.length,
+                        separatorBuilder: ((context, index) {
+                          return kHeight10;
+                        }),
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListileCardChapterWidget(
+                            leading: const Image(
+                                image: NetworkImage(
+                                    "https://media.istockphoto.com/id/926144358/photo/portrait-of-a-little-bird-tit-flying-wide-spread-wings-and-flushing-feathers-on-white-isolated.jpg?b=1&s=170667a&w=0&k=20&c=DEARMqqAI_YoA5kXtRTyYTYU9CKzDZMqSIiBjOmqDNY=")),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 5.h),
                                   child: GooglePoppinsWidgets(
-                                    fontsize: 20.h,
-                                    text: snapshot.data!.docs[index]['title'],fontWeight: FontWeight.bold,
+                                    fontsize: 15.h,
+                                    text: snapshot.data!.docs[index]
+                                        ['chapterName'],
+                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                trailing: InkWell(
+                                Padding(
+                                  padding: EdgeInsets.only(top: 5.h),
                                   child: GooglePoppinsWidgets(
-                                      text: "View",
-                                      fontsize: 16.h,
-                                      fontWeight: FontWeight.w500,
-                                      color: adminePrimayColor),
-                                  onTap: () {
-                                    Navigator.push(context, MaterialPageRoute(builder: (context)=> 
-                                  //  //ViewStudyMaterials(urlPdf: snapshot.data!.docs[index]['downloadUrl']) 
-                                  // PDFViewPage(pdfPath: snapshot.data!.docs[index]['downloadUrl'],)
-                                  //  PDFSectionScreen()
-                                  //PDFSectionScreen(urlPdf: 'https://firebasestorage.googleapis.com/v0/b/dujo-kerala-schools-1a6c5.appspot.com/o/files%2Fstudymaterials%2FEnglish%2FJohny%20Johny%2Ffab221c0-f545-11ed-829a-11b8e7490871?alt=media&token=21f0bd88-b03e-41fd-8267-8a013aa3e80d',) 
-
-                                  PDFSectionScreen(urlPdf: 'https://firebasestorage.googleapis.com/v0/b/dujo-kerala-schools-1a6c5.appspot.com/o/files%2Fstudymaterials%2FEnglish%20%2FTwinkle%20Twinkle%20%2F42725b60-f546-11ed-829a-11b8e7490871?alt=media&token=26fc75c8-8a39-4a24-8f30-804bf3d41a3b',)
-                                  ));
-                                  },                                                    
+                                      fontsize: 15.h,
+                                      text: snapshot.data!.docs[index]
+                                          ['topicName'],
+                                      fontWeight: FontWeight.bold),
                                 ),
-                              );}
-                     ); 
-                  
-               } 
-
-               
-                 else if(snapshot.connectionState == ConnectionState.waiting){
-                    return const Center(child: CircularProgressIndicator(),);
-                    
-                  } 
-                  return const Center(child: Text('No Study Materials Uploaded Yet!'));
-               
-              }
-            ))) ;
-                }
+                                Padding(
+                                  padding: EdgeInsets.only(top: 5.h),
+                                  child: GooglePoppinsWidgets(
+                                      fontsize: 14.h,
+                                      text: 'Pdf',
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                kHeight10,
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    GooglePoppinsWidgets(
+                                        text: "Posted By :", fontsize: 15.h),
+                                    GooglePoppinsWidgets(
+                                      text: snapshot.data!.docs[index]
+                                          ['uploadedBy'],
+                                      fontsize: 15.h,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            title: Padding(
+                              padding: EdgeInsets.only(top: 10.h),
+                              child: GooglePoppinsWidgets(
+                                fontsize: 20.h,
+                                text: snapshot.data!.docs[index]['title'],
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            trailing: InkWell(
+                              child: GooglePoppinsWidgets(
+                                  text: "View",
+                                  fontsize: 16.h,
+                                  fontWeight: FontWeight.w500,
+                                  color: adminePrimayColor),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => PDFSectionScreen(
+                                              urlPdf:
+                                                  'https://firebasestorage.googleapis.com/v0/b/dujo-kerala-schools-1a6c5.appspot.com/o/files%2Fstudymaterials%2FEnglish%20%2FTwinkle%20Twinkle%20%2F42725b60-f546-11ed-829a-11b8e7490871?alt=media&token=26fc75c8-8a39-4a24-8f30-804bf3d41a3b',
+                                            )));
+                              },
+                            ),
+                          );
+                        });
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return const Center(
+                      child: Text('No Study Materials Uploaded Yet!'));
+                })));
   }
-
-  
-  
-
-
-
+}
