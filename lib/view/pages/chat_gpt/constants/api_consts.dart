c0f051dcc427f423818321c9eaef3752163d2c6e
Abin56 <abinjohn8089@gmail.com>
2023-06-30 15:39:53 +0530

    fectching codes from hrithik --30 june friday @ edwin

--- lib/view/pages/chat_gpt/constants/api_consts.dart
+++ lib/view/pages/chat_gpt/constants/api_consts.dart
@@ -1,5 +1,5 @@
 String BASE_URL = "https://api.openai.com/v1/chat";
 String API_KEY = "";
 class APIKey {
-  static const apiKey = "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";
+  static const apiKey = "";
 }
