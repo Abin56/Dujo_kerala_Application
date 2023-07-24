b02200460dd3ffd58d5e51ac8c6771ca3d74bf3a
Abin56 <abinjohn8089@gmail.com>
2023-07-24 15:13:32 +0530

    chat pending -- 24 july --testing git 4

--- lib/view/pages/chat_gpt/constants/api_consts.dart
+++ lib/view/pages/chat_gpt/constants/api_consts.dart
@@ -1,5 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1/chat";
-String API_KEY = "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";
-class APIKey {
-  static const apiKey = "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";
-} 
+String API_KEY = "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";
